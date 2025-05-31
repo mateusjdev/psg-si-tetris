7b9b3238871eaa4a6d64dd729723c7c42a8d0f28
mateusjdev <git@mateusj.dev>
2025-05-31 19:43:26 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace atp_tp_tetris
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double x = 0;
+
+            while (true)
+            {
+                if (Console.KeyAvailable) {
+                    var key = Console.ReadKey();
+                    Console.WriteLine(key);
+                    Console.WriteLine(x);
+                }
+                x++;
+            }
+        }
+    }
+}
